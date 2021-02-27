360edd596461b9c6bf75b96a3bb1a1e340b329c3
Pavel Franta <pavel.franta@zonky.cz>
2021-02-27 21:54:23 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Tailwind from "./Tailwind.svelte";
+  import Login from "./components/login.svelte"
+</script>
+
+<Tailwind />
+<main class="container mx-auto">
+  <Login />
+</main>
---
+++ src/components/login.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Nature from './../assets/nature.svg';
+</script>
+
+<style>
+</style>
+
+<div class="flex justify-items-center">
+  <Nature class="absolute -top-3" />
+</div>
